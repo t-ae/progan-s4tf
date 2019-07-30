5d4c363155ae3e24bf62c0839d6237fcb6072073
araki <araki@qoncept.co.jp>
2019-07-30 17:44:37 +0900

    Replace loss

--- Sources/ProGANModel/Loss.swift
+++ Sources/ProGANModel/Loss.swift
@@ -3,15 +3,20 @@ import TensorFlow
 
 @differentiable
 public func generatorLoss(fakeLogits: Tensor<Float>) -> Tensor<Float> {
-    sigmoidCrossEntropy(logits: fakeLogits,
-                        labels: Tensor(ones: fakeLogits.shape))
+//    sigmoidCrossEntropy(logits: fakeLogits,
+//                        labels: Tensor(ones: fakeLogits.shape))
+    return softplus(-fakeLogits).mean()
 }
 
 @differentiable
 public func discriminatorLoss(realLogits: Tensor<Float>, fakeLogits: Tensor<Float>) -> Tensor<Float> {
-    let realLoss = sigmoidCrossEntropy(logits: realLogits,
-                                       labels: Tensor(ones: realLogits.shape))
-    let fakeLoss = sigmoidCrossEntropy(logits: fakeLogits,
-                                       labels: Tensor(zeros: fakeLogits.shape))
+//    let realLoss = sigmoidCrossEntropy(logits: realLogits,
+//                                       labels: Tensor(ones: realLogits.shape))
+//    let fakeLoss = sigmoidCrossEntropy(logits: fakeLogits,
+//                                       labels: Tensor(zeros: fakeLogits.shape))
+    
+    let realLoss = softplus(-realLogits).mean()
+    let fakeLoss = softplus(fakeLogits).mean()
+    
     return realLoss + fakeLoss
 }
