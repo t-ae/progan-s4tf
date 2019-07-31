e6a9a5ef530b7e4c27419194e9a005ac473d66a4
araki <araki@qoncept.co.jp>
2019-07-31 12:59:34 +0900

    Add test

---
+++ Tests/ProGANModelTests/ComponentTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+import TensorFlow
+import ProGANModel
+
+class ComponentTests: XCTestCase {
+
+    func testMinibatchStdConcatDifferentiability() {
+        let df: (Tensor<Float>)->Tensor<Float> = gradient { x in
+            minibatchStdConcat(x).sum()
+        }
+        
+        let tensor = Tensor<Float>(randomNormal: [16, 32, 32, 128])
+        XCTAssertEqual(df(tensor).shape, tensor.shape)
+    }
+    
+    static let allTests = [
+        ("testMinibatchStdConcatDifferentiability", testMinibatchStdConcatDifferentiability)
+    ]
+}
--- Tests/ProGANModelTests/XCTestManifests.swift
+++ Tests/ProGANModelTests/XCTestManifests.swift
@@ -4,6 +4,7 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(ProGANModelTests.allTests),
+        testCase(ComponentTests.allTests)
     ]
 }
 #endif
