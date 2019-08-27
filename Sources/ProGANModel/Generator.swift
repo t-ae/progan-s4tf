0d20b10f6b6cb101afea377f8f4950383aeebe49
araki <araki@qoncept.co.jp>
2019-08-27 10:50:02 +0900

    Change generator structure

--- Sources/ProGANModel/Generator.swift
+++ Sources/ProGANModel/Generator.swift
@@ -1,71 +1,83 @@
 import Foundation
 import TensorFlow
 
-public struct GeneratorFirstBlock: Layer {
-    var dense: EqualizedDense
-    var conv: EqualizedConv2D
-    
-    public init() {
-        dense = EqualizedDense(inputSize: Config.latentSize,
-                               outputSize: 256*4*4,
-                               activation: lrelu,
-                               gain: sqrt(2)/4)
-        conv = EqualizedConv2D(inputChannels: 256,
-                               outputChannels: 256,
-                               kernelSize: (3, 3),
-                               activation: lrelu)
+struct GBlock: Layer {
+    struct Input: Differentiable {
+        var x: Tensor<Float>
+        @noDerivative
+        var outputRGB: Bool = false
     }
-    
-    @differentiable
-    public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
-        let batchSize = input.shape[0]
-        var x = dense(input)
-        x = x.reshaped(to: [batchSize, 4, 4, 256])
-        x = pixelNormalization(x)
-        x = pixelNormalization(conv(x)) // [batchSize, 256, 4, 4]
-        return x
+    struct Output: Differentiable {
+        var x: Tensor<Float>
+        var rgb: Tensor<Float>
     }
-}
-
-public struct GeneratorBlock: Layer {
     var conv1: EqualizedConv2D
     var conv2: EqualizedConv2D
+    var toRGB: EqualizedConv2D
     
-    public init(inputChannels: Int, outputChannels: Int) {
-        conv1 = EqualizedConv2D(inputChannels: inputChannels,
-                                outputChannels: outputChannels,
-                                kernelSize: (3, 3),
-                                activation: lrelu)
-        conv2 = EqualizedConv2D(inputChannels: outputChannels,
-                                outputChannels: outputChannels,
-                                kernelSize: (3, 3),
-                                activation: lrelu)
-    }
+    @noDerivative
+    let firstBlock: Bool
     
     @differentiable
-    public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
-        var x = input
-        x = resize2xBilinear(images: x)
-        x = pixelNormalization(conv1(x))
+    func callAsFunction(_ input: Input) -> Output {
+        var x = input.x
+        if !firstBlock {
+            x = resize2xBilinear(images: x)
+        }
+        x = conv1(x)
+        if firstBlock {
+            x = x.reshaped(to: [x.shape[0], 4, 4, -1])
+        }
+        x = pixelNormalization(x)
         x = pixelNormalization(conv2(x))
-        return x
+        let rgb: Tensor<Float>
+        if input.outputRGB {
+            rgb = toRGB(x)
+        } else {
+            rgb = Tensor(0)
+        }
+        return Output(x: x, rgb: rgb)
     }
 }
 
 public struct Generator: Layer {
-    public var firstBlock = GeneratorFirstBlock()
+    static let channels = [
+        Config.latentSize, 256, 256, 256, 128, 64, 32, 16
+    ]
     
-    public var blocks: [GeneratorBlock] = []
-    
-    public var toRGB1 = EqualizedConv2D(inputChannels: 1, outputChannels: 1, kernelSize: (1, 1), gain: 1) // dummy at first
-    public var toRGB2 = EqualizedConv2D(inputChannels: 256, outputChannels: 3, kernelSize: (1, 1), gain: 1)
+    var blocks: [GBlock] = []
     
     @noDerivative
     public private(set) var level = 1
     @noDerivative
     public var alpha: Float = 1.0
     
-    public init() {}
+    public init() {
+        let channels = Self.channels
+        let firstBlock = GBlock(
+            conv1: .init(inputChannels: channels[0], outputChannels: channels[1] * 4*4,
+                         kernelSize: (1, 1), padding: .valid, activation: lrelu), // Dense
+            conv2: .init(inputChannels: channels[1], outputChannels: channels[1],
+                         kernelSize: (3, 3), padding: .same, activation: lrelu),
+            toRGB: .init(inputChannels: channels[1], outputChannels: 3,
+                         kernelSize: (1, 1), padding: .valid, activation: lrelu),
+            firstBlock: true
+        )
+        blocks.append(firstBlock)
+        
+        for i in 1..<Config.maxLevel-1 {
+            let block = GBlock(
+                conv1: .init(inputChannels: channels[i], outputChannels: channels[i+1],
+                             kernelSize: (3, 3), padding: .same, activation: lrelu),
+                conv2: .init(inputChannels: channels[i+1], outputChannels: channels[i+1],
+                             kernelSize: (3, 3), padding: .same, activation: lrelu),
+                toRGB: .init(inputChannels: channels[i+1], outputChannels: 3,
+                             kernelSize: (1, 1), padding: .valid, activation: lrelu),
+                firstBlock: false
+            )
+            blocks.append(block)
+        }
+    }
     
     @differentiable
     public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
@@ -73,61 +85,40 @@ public struct Generator: Layer {
         if Config.normalizeLatent {
             x = pixelNormalization(x)
         }
-        x = firstBlock(x)
+        x = x.expandingShape(at: 1, 2)
         
         guard level > 1 else {
             // alpha = 1
-            return toRGB2(x)
+            return blocks[0](.init(x: x, outputRGB: true)).rgb
         }
         
         for lv in 0..<level-2 {
-            x = blocks[lv](x)
+            x = blocks[lv](.init(x: x)).x
         }
         
-        var x1 = x
-        x1 = toRGB1(x1)
-        x1 = resize2xBilinear(images: x1)
+        let out1 = blocks[level-2](.init(x: x, outputRGB: true))
+        let rgb1 = resize2xBilinear(images: out1.rgb)
         
-        var x2 = blocks[level-2](x)
-        x2 = toRGB2(x2)
+        let out2 = blocks[level-1](.init(x: out1.x, outputRGB: true))
+        let rgb2 = out2.rgb
         
-        return lerp(x1, x2, rate: alpha)
+        return lerp(rgb1, rgb2, rate: alpha)
     }
-    
-    static let ioChannels = [
-        (256, 256),
-        (256, 256),
-        (256, 128),
-        (128, 64),
-        (64, 32),
-        (32, 16)
-    ]
     
     public mutating func grow() {
         level += 1
         guard level <= Config.maxLevel else {
             fatalError("Generator.level exceeds Config.maxLevel")
         }
-        
-        let blockCount = blocks.count
-        let io = Generator.ioChannels[blockCount]
-        
-        blocks.append(GeneratorBlock(inputChannels: io.0, outputChannels: io.1))
-        toRGB1 = toRGB2
-        toRGB2 = EqualizedConv2D(inputChannels: io.1, outputChannels: 3, kernelSize: (1, 1), gain: 1)
     }
     
     public func getHistogramWeights() -> [String: Tensor<Float>] {
-        var dict = [
-            "gen\(level)/first.dense": firstBlock.dense.weight,
-            "gen\(level)/first.conv": firstBlock.conv.filter,
-            "gen\(level)/toRGB1": toRGB1.filter,
-            "gen\(level)/toRGB2": toRGB2.filter,
-        ]
+        var dict: [String: Tensor<Float>] = [:]
         
-        for i in 0..<blocks.count {
-            dict["gen\(level)/block\(i).conv1"] = blocks[i].conv1.filter
-            dict["gen\(level)/block\(i).conv2"] = blocks[i].conv2.filter
+        for i in 0..<level {
+            dict["gen/block\(i).conv1"] = blocks[i].conv1.filter
+            dict["gen/block\(i).conv2"] = blocks[i].conv2.filter
+            dict["gen/block\(i).conv2"] = blocks[i].toRGB.filter
         }
         
         return dict
